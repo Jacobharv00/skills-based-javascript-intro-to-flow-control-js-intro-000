6cd041c06bd920b826783b1f428d2552af26c783
Jacob Harvey <jacobharv00@gmail.com>
2021-01-08 02:53:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,17 +7,22 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age >= 13 && age <= 19) {
-    return('You are a teenager!')
+      return('You are a teenager!')
   } else {
-    return('You are not a teenager')
+      return('You are not a teenager')
   }
 }
 
 
-
-
 function ageChecker(age) {
-
+  if(age >= 13 && age <=19) {
+    return('You are a teeenager!')
+  } else if(age <= 12){
+      return('You are a kid')
+  } else {
+    return('You are a grownup')
+  }
+    
 }
 
 function ternaryTeenager(age) {
